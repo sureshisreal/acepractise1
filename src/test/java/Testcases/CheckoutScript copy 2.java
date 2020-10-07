21a0e962e0c15009c6a4362e700f79be46a59558
NithyaRaji <yathnii.93@gmail.com>
2020-10-07 16:00:05 +0530

    latest

---
+++ src/test/java/Testcases/CheckoutScript copy 2.java
@@ -0,0 +1,19 @@
+package Testcases;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import utilities.BaseTest;
+
+public class CheckoutScript extends BaseTest{
+	
+
+	public void Search_Checkout() {
+		homepage.login();
+		
+	}
+	
+	
+	
+}
